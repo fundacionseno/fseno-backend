d7d766fce0cd475928e25c7a920bcf08c9a0106e
fundacionseno <fundacionsenonqn@outlook.com>
2021-07-14 21:18:59 -0300

    se compila build

--- dist/routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const index_controllers_1 = require("../controllers/index.controllers");
-let enrutadorIndex = express_1.Router();
-enrutadorIndex.route('/').get(index_controllers_1.mensaje);
-exports.default = enrutadorIndex;
